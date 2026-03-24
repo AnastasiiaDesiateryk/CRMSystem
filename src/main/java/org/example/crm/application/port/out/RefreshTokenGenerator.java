217d3db0899b742cda160bd0ac22e2d1f746627d
Anastasiia Desiateryk <anastasiiadesiateryk@gmail.com>
2026-03-24 13:51:59 +0100

    Revert "Merge pull request #25 from AnastasiiaDesiateryk/feat/issue-4-refresh-token-rotation"
    This reverts commit 00a2f1b8a506ba43c1de407f62098ff39fde259c, reversing
    changes made to 30e8ea5ab746f846c181c2605a6895ea5428e227.

--- src/main/java/org/example/crm/application/port/out/RefreshTokenGenerator.java
+++
@@ -1,8 +0,0 @@
-package org.example.crm.application.port.out;
-
-
-
-public interface RefreshTokenGenerator {
-    /** cryptographically secure random token for client storage */
-    String generateRaw();
-}
